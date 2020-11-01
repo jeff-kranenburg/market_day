979727d2ed4ef5f63f18f47fb20562b24708ee33
Jeff Kranenburg <jwkranenburg@icloud.com>
2020-11-02 11:07:37 +1300

    Initial Setup

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".chat_on").click(function(){
+        $(".Layout").toggle();
+        $(".chat_on").hide(300);
+    });
+    
+       $(".chat_close_icon").click(function(){
+        $(".Layout").hide();
+           $(".chat_on").show(300);
+    });
+    
+});
